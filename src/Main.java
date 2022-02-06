fc7d90b10fcccff840b4cc62d56cb935db420d4a
DimkaSmile <95303265+DimkaSmile@users.noreply.github.com>
2022-02-06 15:13:38 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15000;
+        int x = 20;
+        int bonusPerMile = price/x;
+        System.out.println(bonusPerMile + " мили");
+    }
+}
